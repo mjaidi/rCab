4c0c283ad939f3e0ad3d4dbb8f29db96ad5e53fb
Ayoub Karioun <kariounayoub@gmail.com>
2019-08-03 16:03:44 +0100

    geocoding ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_03_090834) do
+ActiveRecord::Schema.define(version: 2019_08_03_142147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,10 @@ ActiveRecord::Schema.define(version: 2019_08_03_090834) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "start_lat"
+    t.float "start_lon"
+    t.float "end_lat"
+    t.float "end_lon"
   end
 
   create_table "users", force: :cascade do |t|
