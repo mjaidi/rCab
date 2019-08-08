bc0b5f88a3ee38d35d2f2ef21325a9644c1abbdf
Ayoub Karioun <kariounayoub@gmail.com>
2019-08-09 00:21:21 +0100

    select start end

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,17 @@ Course.destroy_all
 puts 'delete complted'
 puts ''
 
-puts 'creating fake driver'
-User.create(first_name: 'fake', last_name: 'driver', phone: '0600000000', password: 'fakedriver')
+puts 'creating drivers'
+fake_driver = User.create(first_name: 'Fake', last_name: 'Driver', phone: '0600000000', password: 'fakedriver', driver: true)
+driver = User.create(first_name: 'Simo', last_name: 'Hamid', phone: '0612345678', password: 'driverpass', driver: true)
 
 puts 'creating admin'
-User.create(first_name: 'Ayoub', last_name: 'Karioun', phone: '0661000000', password: 'adminpass', admin: true)
+admin = User.create(first_name: 'Hassan', last_name: 'Cherradi', phone: '0661000000', password: 'adminpass', admin: true)
 
-puts 'creating user'
-User.create(first_name: 'Ayoub', last_name: 'Karioun', phone: '0661060260', password: 'clientpass')
+puts 'creating client'
+client = User.create(first_name: 'Ayoub', last_name: 'Karioun', phone: '0661060260', password: 'clientpass')
+
+puts 'creating courses'
+Course.create(start_address: '18 rue miollis 75015', end_address: '11 rue de Javel 75015', price: 10, client: client, driver: fake_driver, status: "search")
+Course.create(start_address: '18 rue miollis 75015', end_address: '82 rue de Lauriston 75116', price: 15, client: client, driver: fake_driver, status: "search")
+
