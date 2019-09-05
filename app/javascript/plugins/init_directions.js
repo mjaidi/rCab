4e6c8b909555d0722872a36dfd25e936cf995b14
Majid Jaidi <majid.jaidi@gmail.com>
2019-09-05 12:23:46 +0100

    added reverse geocoding when setting current location

--- app/javascript/plugins/init_directions.js
+++ app/javascript/plugins/init_directions.js
@@ -1,10 +1,12 @@
 import mapboxgl from "mapbox-gl";
 import MapboxDirections from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.js";
 import style from "./mapbox_style";
+import axios from "axios";
 
 const initMapboxDirections = () => {
   const mapElement = document.getElementById("map-dir");
   if (mapElement) {
+    // initialize map with custom styling
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     let map = new mapboxgl.Map({
       container: "map-dir",
@@ -27,15 +29,48 @@ const initMapboxDirections = () => {
     };
 
     if (mapElement.dataset.withInputs === "true") {
+      // initialize mapbox directions and add input controls
       let directions = new MapboxDirections(options);
       map.addControl(directions, "top-left");
+
+      // initialize gelocation values and names to pass to form
       let originValue = {};
       let destinationValue = {};
       let route = {};
+
+      // workflow for geolication based on current position (get coordinates, reverse geolocate and set directions origin)
+      if ("geolocation" in navigator) {
+        document
+          .getElementById("current-position")
+          .addEventListener("click", e => {
+            navigator.geolocation.getCurrentPosition(p => {
+              reverseGeocode(
+                p.coords.latitude,
+                p.coords.longitude,
+                mapElement.dataset.mapboxApiKey
+              ).then(r => {
+                directions.setOrigin(r);
+              });
+            });
+            document.getElementById("current-position").classList.add("hidden");
+          });
+      }
+
+      //show geolocation button again on origin clear
+      directions.on("clear", e => {
+        if (e.type === "origin") {
+          document
+            .getElementById("current-position")
+            .classList.remove("hidden");
+        }
+      });
+
+      // set name coordinates of origin on origin input
       directions.on("origin", e => {
         const origin = document.querySelector(
           "#mapbox-directions-origin-input input"
         );
+        document.getElementById("current-position").classList.add("hidden");
         originValue = {
           name: origin.value,
           lat: e.feature.geometry.coordinates[1],
@@ -43,17 +78,7 @@ const initMapboxDirections = () => {
         };
       });
 
-      if ("geolocation" in navigator) {
-        document
-          .getElementById("current-position")
-          .addEventListener("click", e => {
-            navigator.geolocation.getCurrentPosition(p => {
-              directions.setOrigin([p.coords.longitude, p.coords.latitude]);
-            });
-            document.getElementById("current-position").classList.add("hidden");
-          });
-      }
-
+      // set name coordinates of destination on destination input
       directions.on("destination", e => {
         const destination = document.querySelector(
           "#mapbox-directions-destination-input input"
@@ -64,6 +89,8 @@ const initMapboxDirections = () => {
           lng: e.feature.geometry.coordinates[0]
         };
       });
+
+      // set form values on route calculation
       directions.on("route", e => {
         document.getElementById("course_start_address").value =
           originValue.name;
@@ -76,6 +103,7 @@ const initMapboxDirections = () => {
         document.getElementById("submit_new_course").classList.remove("hidden");
       });
     } else {
+      // on views without the form initiaize mapbox directions with markers and route but without input boxes
       const markers = JSON.parse(mapElement.dataset.markers);
       options.controls.inputs = false;
       let directions = new MapboxDirections(options);
@@ -93,5 +121,17 @@ const initMapboxDirections = () => {
     }
   }
 };
+
+async function reverseGeocode(lat, lon, token) {
+  let address = "";
+  await axios
+    .get(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${lon},${lat}.json?access_token=${token}`
+    )
+    .then(r => {
+      address = r.data.features[0].place_name;
+    });
+  return address;
+}
 
 export default initMapboxDirections;
